0a9a58465f0ef15783def0b9137da1b4740967b5
Your Name <you@example.com>
2024-07-15 22:23:42 +0700

    add xyw patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=12",
-	"Symbols Nerd Font:pixelsize=15:antialias=true:autohint=true"
+	"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
